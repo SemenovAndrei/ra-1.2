f9984a7cb85fa99f87907bf259126d27afd10098
Andrey Semenov <andss82@gmail.com>
2021-02-06 06:34:40 +0300

    add simple test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />)
+//   const linkElement = screen.getByText(/learn react/i)
+//   expect(linkElement).toBeInTheDocument()
+// })
+
+test('render App', () => {
+  render(<App />)
+  const container = document.querySelector('.container')
+  expect(container).toBeTruthy()
+})
